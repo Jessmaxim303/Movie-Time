4a60cbb05c726593e661d0d864b92474b10a48de
Jesse Maxim <jessmaxim303@gmail.com>
2020-04-02 01:47:28 -0600

    Cards change on hover

--- src/componenets/MovieCard/MovieCard.js
+++ src/componenets/MovieCard/MovieCard.js
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './MovieCard.scss'
 import PropTypes from 'prop-types';
 
-export const MovieCard = ({title, posterPath, id, averageRating, userRating}) => {
-  if (!userRating) {
-    return (
-      <Link className='movie-card' id={id} to={`/movies/${id}`}>
-        <img className='poster-img' alt={`movie poster for ${title}`} src={posterPath} />
-        <h1 className='movie-card-title'>{title}</h1>
-        <footer className='ratings-footer'>
-          <h2 className='movie-rating'>{`Avg. Rating: ${averageRating.toFixed(2)}/10`}</h2>
-        </footer>
-      </Link>
-    )
-  } else {
-    return (
-      <Link className='movie-card' id={id} to={`/movies/${id}`}>
-        <img className='poster-img' alt={`movie poster for ${title}`} src={posterPath} />
-        <h1 className='movie-card-title'>{title}</h1>
-        <footer className='ratings-footer'>
-          <h2 className='movie-rating'>{`Avg. Rating: ${averageRating.toFixed(2)}/10`}</h2>
-          <h2 className='movie-rating user-rating'>{`Your Rating: ${userRating.rating}/10`}</h2>
-        </footer>
-      </Link>
-    )
+export class MovieCard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isHover: false
+    }
+    this.toggleHover = this.toggleHover.bind(this);
   }
-}
 
-MovieCard.propTypes = {
-  title: PropTypes.string,
-  posterPath: PropTypes.string,
-  id: PropTypes.number,
-  averageRating: PropTypes.number,
-  userRating: PropTypes.number
-};
+  toggleHover(e) {
+    e.preventDefault();
+    this.setState(prevState => ({isHovered: !prevState.isHovered}));
+}
+   
+   render() {
+    let moviebox;
+    if (!this.state.isHovered) {
+      moviebox = <Link className='movie-card' id={this.props.id} to={`/movies/${this.props.id}`} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
+          <img className='poster-img' alt={`movie poster for ${this.props.title}`} src={this.props.posterPath} />
+        </Link>
+    } else {
+      moviebox = <Link className='movie-card-detail' id={this.props.id} to={`/movies/${this.props.id}`} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
+          <img className='poster-img' alt={`movie poster for ${this.props.title}`} src={this.props.posterPath} />
+          <h1 className='movie-card-title'>{this.props.title}</h1>
+          <footer className='ratings-footer'>
+          </footer>
+        </Link>
+    }
+
+    return (
+      <section className="moviecard-box">
+        {moviebox}
+      </section>
+      )
+    }
+        
+}
+            // <h2 className='movie-rating'>{`Avg. Rating: ${this.props.averageRating.toFixed(2)}/10`}</h2>
+            // <h2 className='movie-rating user-rating'>{`Your Rating: ${this.props.userRating.rating}/10`}</h2>
